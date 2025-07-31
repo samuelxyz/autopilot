fix off-by-one-timestep error in sensing loop

src/autopilot/kalman.py
@@ -84,11 +84,11 @@ class KalmanFilter:
         assert np.shape(self.A) == np.shape(self.P)
         assert np.shape(self.Q) == np.shape(self.A)
 
-        self.xp = np.full_like(self.x, np.nan)
-        self.Pp = np.full_like(self.P, np.nan)
+        self.xp = self.x.copy()
+        self.Pp = self.P.copy()
         self.K = np.full_like(np.transpose(self.H), np.nan)
 
-    def step(self, A: np.ndarray = None, Q: np.ndarray = None):
+    def predict_step(self, A: np.ndarray = None, Q: np.ndarray = None):
         '''Update the filter by predicting the next state vector (advance one timestep).
         
         Parameters:
@@ -164,7 +164,7 @@ class ExtendedKalmanFilter(KalmanFilter):
         super().__init__(x, P, A, Q, H, R)
         self.zp = np.nan
 
-    def step(self, xp, A=None, Q=None):
+    def predict_step(self, xp, A=None, Q=None):
         '''Update the filter by predicting the next state vector (advance one timestep).
         
         Parameters:
@@ -248,7 +248,7 @@ if __name__ == '__main__':
         })
         A = np.eye(2)
         for i in range(num_steps):
-            kalman_filter.step(A)
+            kalman_filter.predict_step(A)
             kalman_filter.process_observation(noisy_observations[i])
             hist.save_timestep(i)
 

src/autopilot/main.py
@@ -1,8 +1,8 @@
 
-import itertools
 import numpy as np
 import quaternion as quat
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
 
 import ship, thruster, controller, sensor
 import state_def as sd
@@ -46,18 +46,20 @@ def scenario_sensing():
 
     state = sd.make_zero_state()
     state[sd.ANGVEL] = [0.1, 0.1, 0.1]
-    gyro = sensor.Gyroscope(0.01, randseed)
-    star_tracker = sensor.StarTracker(1e-6, 1, randseed)
+    gyro = sensor.Gyroscope(1e-3, randseed)
+    star_tracker = sensor.StarTracker(1e-4, 1, randseed)
     sds = sensor.StateDeterminationSystem(
         [gyro, star_tracker], sd.make_zero_state(), 0.1*np.eye(sd.STATE_N), 
-        1e-4 * np.eye(sd.STATE_N) # if Q is zero then P tends to become singular in the Kalman filter
+        1e-7 * np.eye(sd.STATE_N) # if Q is zero then P tends to become singular in the Kalman filter
     )
     ship_ = ship.Ship(state, 1, np.eye(3), state_det_system=sds)
 
     hist = sim_tools.History(num_steps, {
-        'true_state': ship_.state,
+        'true_state': lambda: ship_.state,
         'est_state': lambda: sds.get_current_state_estimate()[0],
         'est_state_cov': lambda: sds.get_current_state_estimate()[1],
+        # 'est_state': lambda: sds.EKF.x,
+        # 'est_state_cov': lambda: sds.EKF.P,
     })
     sim_tools.run_simulation(time, ship_.update, hist)
 
@@ -65,32 +67,34 @@ def scenario_sensing():
     ang_sd = np.sqrt(hist['est_state_cov'][:, sd.ANGVEL.start, sd.ANGVEL.start])
     ori_true = sd.get_orient(hist['true_state'])
     ori_est = sd.get_orient(hist['est_state'])
-    ori_err = 2*np.acos(quat.as_float_array(ori_true * ori_est.conj())[:, 0]) # radians from est to true
-    ori_component_sd = np.sqrt(np.diagonal(hist['est_state_cov'][:, sd.ORIENT, sd.ORIENT], axis1=-2, axis2=-1))
+    ori_err_quat = quat.as_float_array(ori_true * ori_est.conj())
+    ori_err_angle = 2*np.acos(ori_err_quat[:, 0]) # radians from est to true
+    ori_component_sd = np.sqrt(
+        np.diagonal(hist['est_state_cov'][:, sd.ORIENT, sd.ORIENT], axis1=-2, axis2=-1))
     ori_rss_z = np.linalg.norm(quat.as_float_array(ori_true - ori_est)/ori_component_sd, axis=-1)
-    ori_stdev = ori_err/ori_rss_z
+    ori_stdev = ori_err_angle/ori_rss_z
+    uncertainty_transparency = 0.2
     
     fig, axs = plt.subplots(2, sharex=True)
     ax_ang, ax_ori = axs
     ax_ang.plot(time, hist['true_state'][:, sd.ANGVEL.start], label='True omega_x')
     ax_ang.plot(time, ang_est, label='Est omega_x')
-    ax_ang.fill_between(time, ang_est - ang_sd, ang_est + ang_sd, alpha=0.2, color='C1')
+    ax_ang.fill_between(time, ang_est - ang_sd, ang_est + ang_sd, 
+                        alpha=uncertainty_transparency, color='C1')
     ax_ang.set_ylabel('Angular velocity (rad/s)')
     ax_ang.legend()
     ax_ang.grid()
-    ax_ang.set_ylim(0, 0.2)
-    ax_ori.plot(time, ori_err, label='True Orientation Error')
-    ax_ori.plot(time, ori_stdev, label='Est Orientation Uncertainty')
+    ax_ang.set_ylim(0.09, 0.11)
+    ax_ori.plot(time, ori_err_angle, label='True Orientation Error', color='C1')
+    ax_ori.fill_between(time, 0, ori_stdev, label='Est Orientation Uncertainty', 
+                        color='C1', alpha=uncertainty_transparency)
+    # ax_ori.plot(time, ori_err_quat[:, 1:], label='Error Quat Components')
     ax_ori.grid()
     ax_ori.set_xlabel('Time (s)')
-    ax_ori.set_ylabel('Orientation error (rad)')
-    ax_ori.set_ylim(0, 0.2)
+    ax_ori.set_ylabel('Orientation error')
+    ax_ori.set_ylim(0, 0.005)
+    ax_ori.yaxis.set_major_formatter(tck.EngFormatter('rad'))
     ax_ori.legend()
-    # ori2_color='C1'
-    # ax_or2 = ax_ori.twinx()
-    # ax_or2.semilogy(time, ori_rss_z, color=ori2_color)
-    # ax_or2.set_ylabel('Orientation error z-score\n(Components RMS)', color=ori2_color)
-    # ax_or2.tick_params(axis='y', labelcolor=ori2_color)
     fig.tight_layout()
 
     plt.show()

src/autopilot/sensor.py
@@ -64,6 +64,9 @@ class IntermittentSensor(Sensor):
     
 class Gyroscope(Sensor):
     '''A sensor that directly reads angular velocity in the local frame.'''
+    # SMAD: biases typically 0.003 - 1 deg/hr (1.5e-8 - 5e-6 rad/s), drift in bias varies widely
+    # Mass: <0.1 - 15 kg
+    # Power: <1 to 200 W
 
     def __init__(self, constant_noise, randseed=None):
         super().__init__(3, constant_noise, randseed)
@@ -86,6 +89,9 @@ class Gyroscope(Sensor):
     
 class StarTracker(IntermittentSensor):
     '''An accurate orientation sensor that is only available at certain time intervals.'''
+    # SMAD: 1 arcsec - 1 arcmin (5e-6 - 3e-4 rad)
+    # Mass: 2-5 kg
+    # Power: 5-20 W
 
     # For Kalman filter
     H = np.zeros((4, sd.STATE_N))
@@ -129,9 +135,20 @@ class StarTracker(IntermittentSensor):
         return StarTracker.H
 
 class HorizonSensor(Sensor):
+    # SMAD: Scanning type 0.1 - 0.25 deg (2e-3 - 4e-3 rad) for LEO, down to 0.03 deg (5e-4 rad) in non LEO
+    # Mass: 2-5 kg
+    # Power: 5-10 W
+    
+    # Fixed type for rotating spacecraft: <0.1 - 0.25 deg (2e-3 - 4e-3 rad)
+    # Mass: 0.5 - 3.5 kg
+    # Power: 0.3 - 5 W
+
     pass # TODO
 
 class SunSensor(Sensor):
+    # SMAD: Accuracy 0.005 - 3 deg (1e-4 - 5e-2 rad)
+    # Mass: 0.1 - 2 kg
+    # Power: 0-3 W
     pass # TODO
 
 class GPSReceiver(Sensor):
@@ -141,22 +158,32 @@ class Radar(Sensor):
     pass # TODO
 
 class StateDeterminationSystem:
+    '''Sensor fusion system based around the Extended Kalman Filter.'''
+
     def __init__(self, sensors: list[Sensor], x, P, Q):
         self.sensors = sensors
         self.Q = Q
         self.EKF = kalman.ExtendedKalmanFilter(x, P, Q)
+        self.is_up_to_date = True # Whether self.EKF.x is better to use than self.EKF.xp
 
-    def update(self, true_state, start_time, dt, predicted_accel):
-        active_sensors = [s for s in self.sensors if s.is_available(true_state, start_time)]
+    def predict_step(self, dt, predicted_accel):
+        '''Run the prediction step of the EKF, propagating the estimated state by one timestep.
+        Updates EKF.xp and EKF.Pp, but not EKF.x or EKF.P'''
 
-        # Predict
-        xp, A = self._predict_next_state(dt, predicted_accel)
-        self.EKF.step(xp, A, self.Q)
+        xp = sd.RK4_step(self.EKF.x, dt, predicted_accel)
 
-        # Update
+        A = kalman.make_state_transition_matrix(dt, self.EKF.x)
+        self.EKF.predict_step(xp, A, self.Q)        
+        sd.normalize_quat_part(self.EKF.x)
+        self.is_up_to_date = False
+
+    def sense(self, true_state, timestamp):
+        '''Take a reading from all available sensors, updating the EKF.'''
+        active_sensors = [s for s in self.sensors if s.is_available(true_state, timestamp)]
         H  = np.vstack(tuple(s.linearized_model(self.EKF.xp) for s in active_sensors))
         zp = np.concat(tuple(s.predict_reading(self.EKF.xp) for s in active_sensors)).T
         z  = np.concat(tuple(s.new_reading(true_state) for s in active_sensors)).T
+        
         # assemble R
         R = np.eye(z.size)
         i = 0
@@ -165,16 +192,13 @@ class StateDeterminationSystem:
             inds = slice(i, i+R_sensor.shape[0])
             R[inds, inds] = R_sensor
             i += R_sensor.shape[0]
+
         self.EKF.process_observation(z, zp, H, R)
         sd.normalize_quat_part(self.EKF.x)
-
-    def _predict_next_state(self, dt, predicted_accel):
-        # Could use RK4 but ehh
-        xp = self.EKF.x.copy()
-        xp[sd.VELS] += predicted_accel * dt
-        xp[sd.POS] += xp[sd.VEL] * dt
-        sd.rotate_state(xp, quat.from_rotation_vector(xp[sd.ANGVEL] * dt))
-        return xp, kalman.make_state_transition_matrix(dt, self.EKF.x)
+        self.is_up_to_date = True
 
     def get_current_state_estimate(self):
-        return self.EKF.x, self.EKF.P
+        if self.is_up_to_date:
+            return self.EKF.x, self.EKF.P
+        else:
+            return self.EKF.xp, self.EKF.Pp

src/autopilot/ship.py
@@ -30,49 +30,28 @@ class Ship:
             start_time: time at beginning of timestep
             dt: length of timestep
             '''
+        # Physics
+        self.state = sd.RK4_step(self.state, dt, self.calc_accel)
+        
+        # Predict
         if self.state_det_system is not None:
-            self.state_det_system.update(self.state, start_time, dt, self.calc_accel(self.state))
+             self.state_det_system.predict_step(dt, self.calc_accel)
+
+        # Sense
+        if self.state_det_system is not None:
+            self.state_det_system.sense(self.state, start_time)
             self.state_estimate = self.state_det_system.get_current_state_estimate()
         else:
+            # No sensor system, just use an ideal dummy sensor system
             self.state_estimate = (self.state, np.zeros((self.state.size, self.state.size)))
-        if self.controller is not None:
-            self.controller.update(self.state, dt)
-        self.do_motion_timestep(dt)
-
-    def do_motion_timestep(self, dt):
-        '''Update ship state according to RK4 integration of the equations of motion'''
-        # RK4 ish
-        # Call old conditions a0, v0, q0
-        # Step 1 with dt/2, a0 -> v[0 -> 1] -> q[0 -> 1] -> a1
-        accel0 = self.calc_accel(self.state)
-        state1 = self.state.copy()
-        state1[sd.VELS] += accel0*dt/2
-        state1[sd.POS] += state1[sd.VEL]*dt/2
-        sd.rotate_state(state1, quat.from_rotation_vector(state1[sd.ANGVEL]*dt/2))
-        accel1 = self.calc_accel(state1)
-        # Step 2 with dt/2, a1 -> v[0 -> 2] -> q[0 -> 2] -> a2 
-        state2 = self.state.copy()
-        state2[sd.VELS] += accel1*dt/2
-        state2[sd.POS] += state2[sd.VEL]*dt/2
-        sd.rotate_state(state2, quat.from_rotation_vector(state2[sd.ANGVEL]*dt/2))
-        accel2 = self.calc_accel(state2)
-        # Step 3 with dt, a2 -> v[0 -> 3] -> q[0 -> 3] -> a3
-        state3 = self.state.copy()
-        state3[sd.VELS] += accel2*dt/2
-        state3[sd.POS] += state3[sd.VEL]*dt
-        sd.rotate_state(state3, quat.from_rotation_vector(state3[sd.ANGVEL]*dt/2))
-        accel3 = self.calc_accel(state3)
-        # Step 4 with dt, fractional weights
-        self.state[sd.POS] += dt/6 * (self.state[sd.VEL] + 2*state1[sd.VEL] + 2*state2[sd.VEL] + state3[sd.VEL])
-        sd.rotate_state(self.state, quat.from_rotation_vector(dt/6 * (self.state[sd.ANGVEL] + 2*state1[sd.ANGVEL] + 2*state2[sd.ANGVEL] + state3[sd.ANGVEL])))
-        self.state[sd.VELS] += dt/6 * (accel0 + 2*accel1 + 2*accel2 + accel3)
         
-        # normalize orientation quaternion
-        # self.state[sd.ORIENT] /= np.linalg.norm(self.state[sd.ORIENT])
-        sd.normalize_quat_part(self.state)
+        # Control
+        if self.controller is not None:
+            self.controller.update(self.state_estimate[0], dt)
 
     def calc_accel(self, state) -> np.ndarray:
-        '''Calculates the linear and angular acceleration (total shape (6,)) from actors on the current state'''
+        '''Calculates the linear and angular acceleration (total shape (6,)) 
+        from ship properties and the given state'''
         result = np.zeros((sd.QDOT_N,))
         # rotation_matrix = quat.as_rotation_matrix(sd.get_orient(self.state))
         # world_inertia_matrix = rotation_matrix @ self.inertia_matrix @ rotation_matrix.T